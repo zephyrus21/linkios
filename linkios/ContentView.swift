5257ea52f982ddde9fd21cfb9d10bd096f51b5cd
zephyrus21 <pandey.piyus@gmail.com>
2022-11-13 21:10:57 +0530

    Initial Commit

---
+++ linkios/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  linkios
+//
+//  Created by Piyush Pandey on 13/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ linkios/linkiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  linkiosApp.swift
+//  linkios
+//
+//  Created by Piyush Pandey on 13/11/22.
+//
+
+import SwiftUI
+
+@main
+struct linkiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
