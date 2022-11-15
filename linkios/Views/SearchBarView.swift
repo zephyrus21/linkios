5737b92b2b7d5ae50bea0902373854123b4c8fe1
zephyrus21 <pandey.piyus@gmail.com>
2022-11-15 21:25:20 +0530

    onBoarding view

---
+++ linkios/Views/SearchBarView.swift
@@ -0,0 +1,49 @@
+//
+//  SearchBarView.swift
+//  linkios
+//
+//  Created by Piyush Pandey on 13/11/22.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    var body: some View {
+			HStack(alignment: .center) {
+				Image("demo")
+					.resizable()
+					.edgesIgnoringSafeArea(.all)
+					.aspectRatio(contentMode: .fit)
+					.clipShape(Circle())
+					.frame(width: 50, height: 40)
+				
+				RoundedRectangle(cornerRadius: 8)
+					.foregroundColor(.blue.opacity(0.4))
+					.frame(width: 270, height: 30)
+					.overlay {
+						HStack {
+							Image(systemName: "magnifyingglass")
+								.foregroundColor(.gray)
+							Text("Search")
+								.foregroundColor(.gray)
+								.multilineTextAlignment(.leading)
+								.font(.body)
+							Spacer()
+						}
+						.padding()
+					}
+					Image(systemName: "ellipses.bubble.fill")
+					.resizable()
+					.foregroundColor(.gray)
+					.aspectRatio(contentMode: .fit)
+					.frame(width: 28, height: 28)
+			}
+			.padding(.top)
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBarView()
+    }
+}
